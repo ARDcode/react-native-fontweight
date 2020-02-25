533f87d9892b9f1b1f9a3f1520ee542ef629b834
aynur <adinmukhametov@i-neti.ru>
2020-02-25 17:59:08 +0300

    add italic, delete useless code

--- dist/src/FontManager.js
+++ dist/src/FontManager.js
@@ -1,9 +1,10 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -14,8 +15,8 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
         while (_) try {
-            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [0, t.value];
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
             switch (op[0]) {
                 case 0: case 1: t = op; break;
                 case 4: _.label++; return { value: op[1], done: false };
@@ -34,9 +35,16 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
-}
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_native_1 = require("react-native");
 var react_1 = __importDefault(require("react"));
@@ -146,7 +154,7 @@ var FontManager = /** @class */ (function () {
      *   Used to contain error 'the containing arrow function captures the global'
      *   Therefore swapped it to function instead of arrow to remove global scope
      */
-        var origin = oldRender.call.apply(oldRender, [this].concat(args));
+        var origin = oldRender.call.apply(oldRender, __spreadArrays([this], args));
         if (react_native_1.Platform.OS === 'android') {
             if (origin.props.style) {
                 var fontWeight = origin.props.style.fontWeight ? origin.props.style.fontWeight : '400';
--- src/FontManager.ts
+++ src/FontManager.ts
@@ -1,145 +1,94 @@
-import {Platform, Text} from 'react-native'
-import React from 'react'
+import { Platform, Text } from 'react-native';
+import React from 'react';
 
+type FontStyle = 'normal' | 'italic' | 'oblique';
+type FontWeight = 'normal' | 'bold' | 'bolder' | 'lighter' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900';
 
-type FontStyle = 'normal' | 'italic' | 'oblique'
-type FontWeight =
-	| 'normal'
-	| 'bold'
-	| 'bolder'
-	| 'lighter'
-	| '100'
-	| '200'
-	| '300'
-	| '400'
-	| '500'
-	| '600'
-	| '700'
-	| '800'
-	| '900'
+const getItalicName = (isItalic: boolean) => {
+  if (isItalic) {
+    return 'Italic';
+  }
+  return '';
+};
 
-// FIXME: This function is hideous
-function double_pascal_case_to_two_words(str: string) {
-	let index
-	let count = 0
-	for (let i = 0; i < str.length; i++) {
-		let ch = str.charAt(i)
-		if (ch >= 'A' && ch <= 'Z') {
-			count++
-		}
-		if (count === 2 && !index) {
-			index = i
-		}
-	}
-	if (count === 2) {
-		return str.substr(0, index) + ' ' + str.substring(index, str.length)
-	} else {
-		return str
-	}
-}
+const font_style_generator = (font_family: string, font_weight: FontWeight, font_style: FontStyle) => {
+  let fontFamily = `${font_family}-`;
+  const isItalic: boolean = font_style === 'italic';
+  switch (font_weight) {
+    case 'normal':
+      fontFamily += isItalic ? 'Italic' : 'Regular';
+      break;
+    case 'bold':
+      fontFamily += 'Bold' + getItalicName(isItalic);
+      break;
+    case '100':
+    case '200':
+      fontFamily += 'Thin' + getItalicName(isItalic);
+      break;
+    case '300':
+      fontFamily += 'Light' + getItalicName(isItalic);
+      break;
+    case '400':
+      fontFamily += isItalic ? 'Italic' : 'Regular';
+      break;
+    case '500':
+    case '600':
+      fontFamily += 'Medium' + getItalicName(isItalic);
+      break;
+    case '700':
+    case '800':
+      fontFamily += 'Bold' + getItalicName(isItalic);
+      break;
+    case '900':
+      fontFamily += 'Black' + getItalicName(isItalic);
+      break;
+    case 'bolder':
+    case 'lighter':
+    default:
+      fontFamily += isItalic ? 'Italic' : 'Regular';
+      break;
+  }
 
-function font_style_generator(
-	font_family: string,
-	font_weight: FontWeight,
-	font_style: FontStyle,
-): {fontFamily: string; fontWeight: string} {
-	let fontFamily = `${font_family}-`
+  return { fontFamily: fontFamily, fontWeight: 'normal' };
+};
 
-	switch (font_weight) {
-		case 'normal':
-			fontFamily += 'Regular'
-			break
-		case 'bold':
-			fontFamily += 'Bold'
-			break
-		case '100':
-		case '200':
-			fontFamily += 'Thin'
-			break
-		case '300':
-			fontFamily += 'Light'
-			break
-		case '400':
-			fontFamily += 'Regular'
-			break
-		case '500':
-		case '600':
-			fontFamily += 'Medium'
-			break
-		case '700':
-		case '800':
-			fontFamily += 'Bold'
-			break
-		case '900':
-			fontFamily += 'Black'
-			break
-		case 'bolder':
-		case 'lighter':
-		// @ts-ignore
-		case 'default':
-			fontFamily += 'Regular'
-			break
-	}
-
-	if (font_style === 'italic') {
-		fontFamily += 'Italic'
-	}
-
-	return {fontFamily: fontFamily, fontWeight: 'normal'}
-}
-
-const oldRender = (Text as any).render
+const oldRender = (Text as any).render;
 
 class FontManager {
-	async init(isExpo: boolean = true) {
-		if (isExpo) {
-			const Font = require('expo-font');
-			// @ts-ignore
-			await Font.loadAsync({
-				'Roboto-Black': require('./fonts/Roboto-Black.ttf'),
-				'Roboto-Medium': require('./fonts/Roboto-Medium.ttf'),
-				'Roboto-Bold-Medium': require('./fonts/Roboto-Bold.ttf'),
-				'Roboto-Regular': require('./fonts/Roboto-Regular.ttf'),
-				'Roboto-Thin': require('./fonts/Roboto-Thin.ttf'),
-				'Roboto-Bold': require('./fonts/Roboto-Bold.ttf'),
-				'Roboto-Light': require('./fonts/Roboto-Light.ttf'),
-			});
-		}
-		(Text as any).render = this.override
-	}
+  init = async (isExpo: boolean = true) => {
+    if (isExpo) {
+      const Font = require('expo-font');
+      await Font.loadAsync({
+        'Roboto-Black': require('./fonts/Roboto-Black.ttf'),
+        'Roboto-Medium': require('./fonts/Roboto-Medium.ttf'),
+        'Roboto-Bold-Medium': require('./fonts/Roboto-Bold.ttf'),
+        'Roboto-Regular': require('./fonts/Roboto-Regular.ttf'),
+        'Roboto-Thin': require('./fonts/Roboto-Thin.ttf'),
+        'Roboto-Bold': require('./fonts/Roboto-Bold.ttf'),
+        'Roboto-Light': require('./fonts/Roboto-Light.ttf'),
+      });
+    }
+    (Text as any).render = this.override;
+  };
 
-	override(...args) {
-		/* FIXME: Determine if this was the correct thing to do
-     *   Original code  ->  const origin = oldRender.call(this, ...args);
-     *   Used to contain error 'the containing arrow function captures the global'
-     *   Therefore swapped it to function instead of arrow to remove global scope
-     */
-		const origin = oldRender.call(this, ...args)
+  override = (...args: any) => {
+    const origin = oldRender.call(this, ...args);
 
-		if (Platform.OS === 'android') {
-			if (origin.props.style) {
-				const fontWeight: FontWeight = origin.props.style.fontWeight ? origin.props.style.fontWeight : '400'
+    if (Platform.OS === 'android') {
+      if (origin.props.style) {
+        const fontWeight: FontWeight = origin.props.style.fontWeight ? origin.props.style.fontWeight : '400';
 
-				const fontStyle: FontStyle = origin.props.style.fontStyle ? origin.props.style.fontStyle : 'normal'
-				// HACK: Disabled mutation of fontStyle as is immutable in some libaries
-				// origin.props.style.fontStyle = 'normal'
+        const fontStyle: FontStyle = origin.props.style.fontStyle ? origin.props.style.fontStyle : 'normal';
 
-				const fontFamily: string = origin.props.style.fontFamily ? origin.props.style.fontFamily : 'Roboto'
+        const fontFamily: string = origin.props.style.fontFamily ? origin.props.style.fontFamily : 'Roboto';
 
-				return React.cloneElement(origin, {
-					style: [{}, origin.props.style, font_style_generator(fontFamily, fontWeight, fontStyle)],
-				})
-			}
-			return origin
-		} else {
-			if (origin.props.style) {
-				if (origin.props.style.fontFamily) {
-					const fontFamily: string = origin.props.style.fontFamily
-					origin.props.style.fontFamily = double_pascal_case_to_two_words(fontFamily)
-				}
-			}
-			return origin
-		}
-	}
+        return React.cloneElement(origin, {
+          style: [{}, origin.props.style, font_style_generator(fontFamily, fontWeight, fontStyle)],
+        });
+      }
+      return origin;
+    }
+    return origin;
+  };
 }
-export default new FontManager()
+export default new FontManager();
